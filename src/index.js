2fe6c4fc967bd69d5c3246b632781a868ff75e1d
Viktoria <colosova28@yandex.ru>
2024-10-06 00:30:55 +0300

    настройка линтеров

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './styles/style.css';
+
+document.querySelector('app').innerHTML = `
+  <div>
+
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
+import eslint from 'vite-plugin-eslint2';
 
 export default defineConfig({
-  plugins: [handlebars()],
-}) 
+  plugins: [handlebars(), eslint()]
+});
