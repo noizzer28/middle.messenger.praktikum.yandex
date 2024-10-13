c67b4cfcf9dd9a9199ea47492427f0487ae126f9
Viktoria <colosova28@yandex.ru>
2024-10-13 19:26:34 +0300

    correction

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import handlebars from 'vite-plugin-handlebars';
 import eslint from 'vite-plugin-eslint2';
 
 export default defineConfig({
-  base: '/login',
+  base: '/',
   server: {
     port: 3000
   },
