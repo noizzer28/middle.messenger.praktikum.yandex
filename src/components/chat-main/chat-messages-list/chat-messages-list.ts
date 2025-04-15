40569a952b83fdedc6a4dae004ee3533c7e6cc20
Viktoria <colosova28@yandex.ru>
2025-04-15 14:54:29 +0300

    Правки

--- src/components/chat-main/chat-messages-list/chat-messages-list.ts
+++ src/components/chat-main/chat-messages-list/chat-messages-list.ts
@@ -13,29 +13,4 @@ class ChatMessagesList extends Block {
   }
 }
 
-// function mapStateToMessenger(state: TStore) {
-//   const activeMessages = state.activeMessages;
-//   const userId = state.user?.id;
-//   if (activeMessages.length > 0) {
-//     // return activeMessages.map((message) => {
-//     //   const isCurrentUser = message.user_id === userId ? true : null;
-//     //   return {
-//     //     you: isCurrentUser,
-//     //     text: message.content,
-//     //     time: formatDate(message.time)
-//     //   };
-//     // });
-//     return {
-//       text: 'message text'
-//     };
-//   } else {
-//     return {
-//       text: 'Здесь нет сообщений'
-//     };
-//   }
-// }
-
-// const Connect = connect(ChatMessagesList, mapStateToMessenger);
-// export const chatMessagesList = new Connect('main', {});
-
 export default ChatMessagesList;
